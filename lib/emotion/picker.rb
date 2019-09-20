7f3f9616e1292fd8dc5019e412d38aaa5f2f6330
Christopher Birmingham <chris.birmingham@hotmail.co.uk>
2019-09-20 23:12:58 +0100

    frozen_string_literal
    Updated the codebase to use frozen_string_literal so I don't have to
    use .freeze for string constants

--- lib/emotion/picker.rb
+++ lib/emotion/picker.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'emotion/picker/version'
 require 'emotion/picker/emotion'
 require 'yaml'
 
 module Emotion
   module Picker
-    EMOTIONS_FILE = "#{__dir__}/emotions.yml".freeze
+    EMOTIONS_FILE = "#{__dir__}/emotions.yml"
     private_constant :EMOTIONS_FILE
 
     # Class for choosing an emotion
--- lib/emotion/picker/emotion.rb
+++ lib/emotion/picker/emotion.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Emotion
   module Picker
     # DAO class for representing an emotion
